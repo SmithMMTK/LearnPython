6318f5478697cba5c0093759cc820bf020a3cc18
SmithMMTK <smithm@hotmail.co.th>
2023-09-01 14:46:19 +0700

    Restructure folder

--- Lessons/Week-01/Lesson-03.py
+++ Lessons/Week-01/Lesson-03.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Open the image file name "bird.jpg" from the subfolder "Lessons\Lesson-01 Image Processing"
-image=Image.open("Lessons\\Lesson-01 Image Processing\\bird.jpg")
+image=Image.open("Lessons/images/bird.jpg")
 
 # Get the pixel value of the image
 pixel_data = list(image.getdata())
