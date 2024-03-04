d2a3d8066599b09b890476cdb4f144cda944f9c9
SmithMMTK <smithm@hotmail.co.th>
2024-03-04 08:43:18 +0700

    Update password.py

--- Lessons/Stock/password.py
+++ Lessons/Stock/password.py
@@ -1,17 +1,24 @@
-# Define a dictionary to contain website and password pairs
+## Represent data store to keep track of passwords for different websites
 Secret = {
     "thiswebsite.com": "password123",
     "thatwebsite.com": "securepassword"
 }
 
-# Accessing the password for a specific website
-website = "thiswebsite.com"
-password = Secret.get(website)
+
+## Function to validate that there is a password for a given website
+def Exists(secretArray,char):
+    # Check if the website exists in the secretArray
+    # Loop index through the secretArray from [0] to [n]
+    for i in secretArray:
+        # If the website exists, return True
+        if i == char:
+            return True
 
 
-if password is not None:
-    print(f"The password for {website} is: {password}")
+    return False
+
+
+if Exists(Secret,"thiswebsite.com"):
+    print("Password exists in Secret")
 else:
-    print(f"No password found for {website}")
-
-
+    print("Password does not exist Secret")
