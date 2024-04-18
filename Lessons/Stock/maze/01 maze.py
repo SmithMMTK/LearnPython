1bf4aa093ecc99fdb0c9d2e2294d3d85ab18b070
SmithMMTK <smithm@hotmail.co.th>
2024-04-18 15:28:33 +0700

    maze project

---
+++ Lessons/Stock/maze/01 maze.py
@@ -0,0 +1,61 @@
+
+# Template for the maze project with basic functions
+import time
+import os 
+
+# Render the maze function
+def render_maze(maze):
+    print("======================")
+    print(" Maze to solve ")
+    print("======================")
+    for row in maze:
+        print(' '.join([str(cell) for cell in row]))
+
+# Test render_maze
+maze = [
+    [1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+    [1, 1, 1, 0, 1, 0, 0, 1, 0, 1],
+    [0, 1, 0, 0, 1, 1, 1, 1, 0, 0],
+    [0, 1, 0, 1, 1, 0, 0, 1, 1, 1],
+    [0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    [0, 0, 0, 1, 0, 1, 0, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 0, 1, 1, 0],
+    [0, 1, 1, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 0, 1, 0, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 1, 0, 0, 0, 1, 0]
+]
+
+# Print the steps of the path
+def print_path(path):
+    # Step through each position in the path
+    for position in path:
+        # Clear the console
+        os.system('cls' if os.name == 'nt' else 'clear')
+        
+        print("======================")
+        print(" Path to solve ")
+        print("======================")
+        
+        for row in range(len(maze)):
+            for col in range(len(maze[0])):
+                if [row, col] == position:
+                    print('\033[92m' + 'P' + '\033[0m', end=' ')
+                else:
+                    if maze[row][col] == 1:
+                        print('1', end=' ')  # Unvisited path cells
+                    else:
+                        print('0', end=' ')  # Walls
+            print()
+        time.sleep(0.5)  # Delay for 0.5 seconds
+
+# Define the start and end points
+start = (0, 0)
+end = (9, 9)
+
+# Define array to store the path
+path = [[0, 0], [1, 0], [1,1],[2,1],[3,1],[4,1]]  # Dummy path for demonstration
+
+
+
+print_path(path)
+
