775102d7f8654e501c961214aded25e061b583b6
SmithMMTK <smithm@hotmail.co.th>
2024-07-21 12:18:13 +0700

    Square Root problem

---
+++ Lessons/Stock/sqrRoot.py
@@ -0,0 +1,33 @@
+# Find square root of a number by dividing the number by 2 on target digit
+
+def sqrRoot(target):
+    if target == 0:
+        return 0
+    elif target < 0:
+        return -1
+
+    left = 0
+    right = 999
+    previousAnswer = 0
+
+    while True:
+        mid = (left + right) / 2
+        if mid == previousAnswer:
+            return mid
+        else:
+            previousAnswer = mid
+        print("mid: ", mid)
+        if mid * mid == target:
+            return mid
+        elif mid * mid < target:
+                left = mid
+        else:
+                right = mid
+
+    # Check the digit length of current
+
+
+target = 10
+answer = sqrRoot(target)
+
+#print(sqrRoot(target, current, left, right))
---
+++ Lessons/Stock/sqrRootXY.py
@@ -0,0 +1,42 @@
+# Find square root of a number by dividing the number by 2 on target digit
+
+def sqrRoot(target,y):
+    if target == 0:
+        return 0
+    elif target < 0:
+        return -1
+    
+    if y < 2:
+        return -1
+
+    left = 0
+    right = 10
+    previousAnswer = 0
+    
+
+    while True:
+        mid = (left + right) / 2
+        if mid == previousAnswer:
+            return mid
+        else:
+            previousAnswer = mid
+        print("mid: ", mid)
+        midValue = 1
+        ## Multiply mid by itself y times
+        for i in range(0,y):
+            midValue = midValue * mid
+
+        if midValue == target:
+            return mid
+        elif midValue < target:
+                left = mid
+        else:
+                right = mid
+
+    # Check the digit length of current
+
+
+target = 9
+answer = sqrRoot(target,3)
+
+#print(sqrRoot(target, current, left, right))
